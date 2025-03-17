726c2725e2ab3e9911359d6fc353df8532949f2d
guiitha <www.guitha@gmail.com>
2025-03-17 19:29:59 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://wwwguitha:8efOVdvuPftkNt7j@my-project-database.2jau5.mongodb.net/?retryWrites=true&w=majority&appName=my-project-database', // Change to your DB URL
+      database: 'my-project-database',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
